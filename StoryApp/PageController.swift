9dfde1375c9f5c47fea388a7a6cb8f9f27d72fbd
vtboyarc <adamrcarter@gmail.com>
2017-04-11 17:27:08 -0500

    hooked up the buttons, and did some refactoring

--- StoryApp/PageController.swift
+++ StoryApp/PageController.swift
@@ -14,12 +14,41 @@ class PageController: UIViewController {
     
     //MARK: - Views
     
-    let artworkView = UIImageView()
-    let storyLabel = UILabel()
-    let firstChoiceButton = UIButton(type: .system)
-    let secondChoiceButton = UIButton(type: .system)
+    let artworkView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+        
+    }()
     
     
+    let storyLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    
+    
+    let firstChoiceButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+        
+    }()
+    
+    let secondChoiceButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+        
+    }()
+
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -33,9 +62,32 @@ class PageController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .white
+        
         if let page = page {
             artworkView.image = page.story.artwork
-            storyLabel.text = page.story.text
+            
+            let attributedString = NSMutableAttributedString(string: page.story.text)
+            
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.lineSpacing = 10
+            
+            attributedString.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: NSMakeRange(0, attributedString.length))
+            
+            storyLabel.attributedText = attributedString
+            
+            if let firstChoice = page.firstChoice {
+                firstChoiceButton.setTitle(firstChoice.title, for: .normal)
+                firstChoiceButton.addTarget(self, action: #selector(PageController.loadFirstChoice), for: .touchUpInside)
+            } else {
+                firstChoiceButton.setTitle("Play again", for: .normal)
+                firstChoiceButton.addTarget(self, action: #selector(PageController.playAgain), for: .touchUpInside)
+            }
+            
+            if let secondChoice = page.secondChoice {
+                secondChoiceButton.setTitle(secondChoice.title, for: .normal)
+                secondChoiceButton.addTarget(self, action: #selector(PageController.loadSecondChoice), for: .touchUpInside)
+            }
         }
     }
 
@@ -48,7 +100,6 @@ class PageController: UIViewController {
         super.viewWillLayoutSubviews()
         
         view.addSubview(artworkView)
-        artworkView.translatesAutoresizingMaskIntoConstraints = false
         
         artworkView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         
@@ -60,16 +111,51 @@ class PageController: UIViewController {
             ])
         
         view.addSubview(storyLabel)
-        //allows the system to determine how many lines it needs to use to display all the text
-        storyLabel.numberOfLines = 0
-        storyLabel.translatesAutoresizingMaskIntoConstraints = false
-        
+      
         NSLayoutConstraint.activate([
             storyLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16.0),
             storyLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16.0),
             storyLabel.topAnchor.constraint(equalTo: view.centerYAnchor, constant: -48.0)
         ])
+        
+        view.addSubview(firstChoiceButton)
+        
+        NSLayoutConstraint.activate([
+            firstChoiceButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            firstChoiceButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -80.0)
+            ])
+        
+        view.addSubview(secondChoiceButton) 
+        
+        NSLayoutConstraint.activate([
+            secondChoiceButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            secondChoiceButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -32)
+            ])
+    
     }
+    
+    func loadFirstChoice() {
+        if let page = page, let firstChoice = page.firstChoice {
+            let nextPage = firstChoice.page
+            let pageController = PageController(page: nextPage)
+            
+            navigationController?.pushViewController(pageController, animated: true)
+        }
+    }
+    
+    func loadSecondChoice() {
+        if let page = page, let secondChoice = page.secondChoice {
+            let nextPage = secondChoice.page
+            let pageController = PageController(page: nextPage)
+            navigationController?.pushViewController(pageController, animated: true)
+        }
+    }
+    
+    func playAgain() {
+        navigationController?.popToRootViewController(animated: true)
+        
+    }
+    
 }
 
 
