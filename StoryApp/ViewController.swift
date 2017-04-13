65d9b7c7a8c2c49a4ac723a1a7268c427b84b1dc
vtboyarc <adamrcarter@gmail.com>
2017-04-13 18:14:52 -0500

    Added text input for adding name

--- StoryApp/ViewController.swift
+++ StoryApp/ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var textFieldBottomConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: Notification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
         
     }
 
@@ -22,12 +28,61 @@ class ViewController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "startAdventure" {
-            guard let pageController = segue.destination as? PageController else {
-                return }
             
-            //Adventure is the struct in Page.swift
-            pageController.page = Adventure.story
+            do {
+                if let name = nameTextField.text {
+                    if name == "" {
+                        throw AdventureError.nameNotProvided
+                    } else {
+                        guard let pageController = segue.destination as? PageController else {
+                            return }
+                        //Adventure is the struct in Page.swift
+                        pageController.page = Adventure.story(withName: name)
+                    }
+                }
+            } catch AdventureError.nameNotProvided {
+                let alertController = UIAlertController(title: "Name Not Provided", message: "Provide a name to start the story", preferredStyle: .alert)
+                
+                let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alertController.addAction(action)
+                
+                present(alertController, animated: true, completion: nil)
+                
+                
+            } catch let error {
+                fatalError("\(error.localizedDescription)")
+            }
         }
+    }
+    
+    func keyboardWillShow(_ notification: Notification) {
+        if let info = notification.userInfo, let keyboardFrame = info[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+            let frame = keyboardFrame.cgRectValue
+            //adds an extra 10 pixels of height to the text input box
+            textFieldBottomConstraint.constant = frame.size.height + 10
+            
+            UIView.animate(withDuration: 0.8) {
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    func keyboardWillHide(_ notification: Notification) {
+        textFieldBottomConstraint.constant = 40
+        
+        UIView.animate(withDuration: 0.8) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
